f5491eab07a32d050ee915815cd3b3e3cb53ac4e
ksc1008 <ksc1008@naver.com>
2025-12-13 20:54:14 +0900

    [fix] 잘못된 Annotation 수정

--- app/src/main/java/com/ssavice/ssavice/MainActivity.kt
+++ app/src/main/java/com/ssavice/ssavice/MainActivity.kt
@@ -12,9 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ssavice.ssavice.ui.theme.SsaviceTheme
-import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.AndroidEntryPoint
 
-@HiltAndroidApp
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
