4579c1021c4a5ee60ab753c8401af6cd41dc0a4d
ksc1008 <ksc1008@naver.com>
2025-12-13 20:49:57 +0900

    [refactor] MainActivity에 Hilt Annotation 적용

--- app/src/main/java/com/ssavice/ssavice/MainActivity.kt
+++ app/src/main/java/com/ssavice/ssavice/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ssavice.ssavice.ui.theme.SsaviceTheme
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
