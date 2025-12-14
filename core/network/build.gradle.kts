d16152f1fbad38a218550286b6193bfd1b947e38
ksc1008 <ksc1008@naver.com>
2025-12-15 05:08:09 +0900

    [chore] network 모듈에 의존성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
     alias(libs.plugins.ssavice.android.library)
     alias(libs.plugins.ssavice.hilt)
+    id("kotlinx-serialization")
 }
 
 android {
     namespace = "com.ssavice.core.network"
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
 }
