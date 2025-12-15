cafac0cada2910ab1c4937c068ebb98323f345a1
ksc1008 <ksc1008@naver.com>
2025-12-15 12:43:33 +0900

    [feat] JWT 관련 인터페이스 추가

---
+++ core/network/src/main/java/com/ssavice/network/model/JWT.kt
@@ -0,0 +1,14 @@
+package com.ssavice.network.model
+
+import android.annotation.SuppressLint
+import kotlinx.serialization.Serializable
+
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
+data class JWT(
+    val expirationTick: Long,
+    val refreshToken: String,
+    val accessToken: String
+) {
+    fun isExpired(): Boolean = false
+}
