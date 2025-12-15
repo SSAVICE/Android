ee7858f7d8c135fbb49fbb22ad83dba22489a349
ksc1008 <ksc1008@naver.com>
2025-12-15 12:41:40 +0900

    [chore] 직렬화, BuildConfig에 필요한 의존성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,16 +1,40 @@
+import com.android.build.api.variant.BuildConfigField
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.ssavice.android.library)
     alias(libs.plugins.ssavice.hilt)
-    id("kotlinx-serialization")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
+    buildFeatures {
+        buildConfig = true
+    }
     namespace = "com.ssavice.core.network"
 }
 
+val localProps = providers
+    .fileContents(rootProject.layout.projectDirectory.file("local.properties"))
+    .asText
+    .map {
+        Properties().apply { load(it.reader())}
+    }
+
+val backendURL = localProps.map { it.getProperty("BACKEND_URL") }
+    .orElse("http://example.com")
+
 dependencies {
+    implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
-    implementation(libs.retrofit.kotlin.serialization)
+}
+
+androidComponents {
+    onVariants {
+        it.buildConfigFields!!.put("BACKEND_URL", backendURL.map { value ->
+            BuildConfigField(type = "String", value = """"$value"""", comment = null)
+        })
+    }
 }
