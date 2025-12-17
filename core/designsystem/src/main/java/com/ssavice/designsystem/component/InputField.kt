c05660434ed3c1643dba2c83a1fd6b3f22c05d5e
ksc1008 <ksc1008@naver.com>
2025-12-17 21:54:28 +0900

    [feat] MultiLineInputField 컴포넌트 추가, 하나의 함수로 변경

--- core/designsystem/src/main/java/com/ssavice/designsystem/component/InputField.kt
+++ core/designsystem/src/main/java/com/ssavice/designsystem/component/InputField.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldLabelPosition
 import androidx.compose.material3.Typography
@@ -19,40 +21,41 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ssavice.designsystem.theme.SsaviceLightGray
 import com.ssavice.designsystem.theme.SsaviceTheme
 
 @Composable
 fun SsaviceInputField(
     state: TextFieldState,
     modifier: Modifier = Modifier,
-    placeholderText: String? = null
-) {
-    OutlinedTextField(
-        state = state,
-        modifier = modifier,
-        shape = RoundedCornerShape(12),
-        lineLimits = TextFieldLineLimits.SingleLine,
-        placeholder = {PlaceHolderText(placeholderText?:"")},
-        contentPadding = PaddingValues(12.dp),
-    )
-}
-
-@Composable
-fun LabeledSsaviceInputField(
-    state: TextFieldState,
-    modifier: Modifier = Modifier,
     placeholderText: String? = null,
-    labelText: String? = null
+    labelText: String? = null,
+    multiLine: Boolean = false,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    enabled: Boolean = true
 ) {
+    val shape = RoundedCornerShape(8.dp)
     OutlinedTextField(
         state = state,
         modifier = modifier,
-        shape = RoundedCornerShape(12),
-        lineLimits = TextFieldLineLimits.SingleLine,
-        placeholder = {PlaceHolderText(placeholderText?:"")},
+        shape = shape,
+        lineLimits =
+            if (multiLine) TextFieldLineLimits.SingleLine
+            else TextFieldLineLimits.MultiLine(
+                1,
+                4
+            ),
+        placeholder = { PlaceHolderText(placeholderText ?: "") },
         contentPadding = PaddingValues(12.dp),
-        label = { LabelText(labelText ?: "") },
-        labelPosition = TextFieldLabelPosition.Above()
+        label = if (labelText != null) {
+            { LabelText(labelText) }
+        } else null,
+        labelPosition = TextFieldLabelPosition.Above(),
+        keyboardOptions = keyboardOptions,
+        enabled = enabled,
+        colors = OutlinedTextFieldDefaults.colors().copy(
+            unfocusedIndicatorColor = SsaviceLightGray
+        )
     )
 }
 
@@ -72,6 +75,7 @@ private fun InputFieldPreview() {
         }
     }
 }
+
 @Preview
 @Composable
 private fun LabeledInputFieldPreview() {
@@ -80,8 +84,12 @@ private fun LabeledInputFieldPreview() {
     }
     SsaviceTheme {
         SsaviceBackground(modifier = Modifier.size(200.dp, 150.dp)) {
-            Box(modifier = Modifier.size(170.dp, 120.dp).padding(10.dp)) {
-                LabeledSsaviceInputField(
+            Box(
+                modifier = Modifier
+                    .size(170.dp, 120.dp)
+                    .padding(10.dp)
+            ) {
+                SsaviceInputField(
                     state = state,
                     placeholderText = "홍길동",
                     labelText = "이름"
@@ -91,17 +99,46 @@ private fun LabeledInputFieldPreview() {
     }
 }
 
+@Preview
+@Composable
+private fun LabeledInputFieldMultilinePreview() {
+    val state = remember {
+        TextFieldState(
+            "ABCDEFG\n대한민국 최고의 중식 전문점 최고의 맛을 보장합니다.\n예약문의: 010-1234-5678\n주차가능"
+        )
+    }
+    SsaviceTheme {
+        SsaviceBackground(modifier = Modifier.size(350.dp, 600.dp)) {
+            Box(
+                modifier = Modifier
+                    .size(170.dp, 120.dp)
+                    .padding(10.dp)
+            ) {
+                SsaviceInputField(
+                    state = state,
+                    placeholderText = "홍길동",
+                    labelText = "설명"
+                )
+            }
+        }
+    }
+}
+
 @Composable
 private fun PlaceHolderText(text: String) {
-    Text(text = text,
+    Text(
+        text = text,
         style = Typography().bodyMedium,
         modifier = Modifier
             .wrapContentHeight(align = Alignment.CenterVertically),
-        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f))
+        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f)
+    )
 }
 
 @Composable
 private fun LabelText(text: String) {
-    Text(text = text,
-        style = Typography().labelSmall)
+    Text(
+        text = text,
+        style = Typography().labelSmall
+    )
 }
--- core/designsystem/src/main/java/com/ssavice/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/ssavice/designsystem/theme/Color.kt
@@ -5,4 +5,5 @@ import androidx.compose.ui.graphics.Color
 internal val SsaviceOrange = Color(0xFFC84F33)
 internal val SsaviceLightOrange = Color(0xFFB9563D)
 internal val SsaviceGray = Color(0xFF717171)
+internal val SsaviceLightGray = Color(0xFFADADAD)
 internal val SsaviceSurfaceGray = Color(0xFFFCFCFC)
