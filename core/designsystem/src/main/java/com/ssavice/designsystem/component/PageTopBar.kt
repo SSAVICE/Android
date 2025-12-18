e1aacf191f97f72846bb2d92400edc1b99ab229c
ksc1008 <ksc1008@naver.com>
2025-12-18 12:25:46 +0900

    [feat] 추후 사용할 앱 상단 바 추가

---
+++ core/designsystem/src/main/java/com/ssavice/designsystem/component/PageTopBar.kt
@@ -0,0 +1,19 @@
+package com.ssavice.designsystem.component
+
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun SsaviceTopBar(
+    title: String,
+    rightButtonVisible: Boolean,
+    rightButtonIcon: ImageVector,
+    rightButtonIconContentDescription: String,
+    modifier: Modifier = Modifier,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors()
+    ) {
+
+}
