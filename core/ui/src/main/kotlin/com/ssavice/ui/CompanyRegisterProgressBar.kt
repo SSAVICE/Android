f02cad328bbe96294a0a9cf9264474d6eb29eb2c
ksc1008 <ksc1008@naver.com>
2025-12-18 09:11:15 +0900

    [feat] 회사 등록 progress bar 상수값 분리, 스텝 표시 기능 추가

--- core/ui/src/main/kotlin/com/ssavice/ui/CompanyRegisterProgressBar.kt
+++ core/ui/src/main/kotlin/com/ssavice/ui/CompanyRegisterProgressBar.kt
@@ -3,6 +3,7 @@ package com.ssavice.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,9 +12,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ssavice.designsystem.component.SsaviceBackground
@@ -25,8 +30,12 @@ fun WideBar(
     modifier: Modifier = Modifier
 ) {
     Box(
-       modifier = modifier.height(6.dp)
-           .background(if(active) MaterialTheme.colorScheme.primary else Color.LightGray, RoundedCornerShape(12.dp))
+        modifier = modifier
+            .height(6.dp)
+            .background(
+                if (active) MaterialTheme.colorScheme.primary else Color.LightGray,
+                RoundedCornerShape(12.dp)
+            )
     )
 }
 
@@ -34,15 +43,30 @@ fun WideBar(
 fun ProgressBar(
     maxStep: Int,
     currentStep: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    showStep: Boolean = false,
 ) {
-    Row(modifier = modifier.padding(10.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-        for(i in 0 until maxStep) {
-            WideBar(
-                active = i < currentStep,
-                modifier = Modifier.weight(1f)
-            )
+    Column {
+        Row(
+            modifier = modifier.padding(
+                horizontal = RegisterProgressBarDefaults.HORIZONTAL_PADDING, vertical = 5.dp
+            ),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            for (i in 0 until maxStep) {
+                WideBar(
+                    active = i < currentStep,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+        if (showStep) {
+            ProvideTextStyle(value = MaterialTheme.typography.labelSmall) {
+                Text(
+                    modifier = Modifier.padding(start = RegisterProgressBarDefaults.HORIZONTAL_PADDING).alpha(0.6f),
+                    text = "${RegisterProgressBarDefaults.STRING_STEP} $currentStep / $maxStep",
+                )
+            }
         }
     }
 }
@@ -55,9 +79,15 @@ fun WideBarPreview() {
         SsaviceBackground(Modifier.size(width = 400.dp, height = 300.dp)) {
             Box(modifier = Modifier.fillMaxSize()) {
                 ProgressBar(
-                    3, 0, modifier = Modifier.fillMaxWidth()
+                    3, 0, modifier = Modifier.fillMaxWidth(),
+                    showStep = true
                 )
             }
         }
     }
+}
+
+internal object RegisterProgressBarDefaults {
+    const val STRING_STEP = "Step"
+    val HORIZONTAL_PADDING = 10.dp
 }
