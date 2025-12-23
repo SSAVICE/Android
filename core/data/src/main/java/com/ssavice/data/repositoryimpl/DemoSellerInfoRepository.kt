afffa5eef2c97bfc4f49b9a4c41feaf9f65d1341
ksc1008 <ksc1008@naver.com>
2025-12-23 19:48:13 +0900

    [feat] 서버를 거치지 않고 데모 정보 불러오는 데모 리포지토리 구현

---
+++ core/data/src/main/java/com/ssavice/data/repositoryimpl/DemoSellerInfoRepository.kt
@@ -0,0 +1,53 @@
+package com.ssavice.data.repositoryimpl
+
+import com.ssavice.data.repository.SellerInfoRepository
+import com.ssavice.model.SellerInfo
+import com.ssavice.model.SellerMainInfo
+import com.ssavice.model.Service
+import java.lang.Thread.sleep
+import java.net.URL
+import java.time.LocalDateTime
+import javax.inject.Inject
+import kotlin.random.Random
+
+internal class DemoSellerInfoRepository @Inject
+constructor(): SellerInfoRepository {
+    override suspend fun registerSellerInformation(sellerInfo: SellerInfo): Result<Unit> {
+        sleep(500)
+        return Result.success(Unit)
+    }
+
+    override suspend fun getSellerInformation(sellerId: Long): Result<SellerMainInfo> {
+        fun createService(name: String, tags: List<String> = listOf("A", "B", "C")) = Service(
+            id = Random(LocalDateTime.now().second).nextLong(),
+            name = name,
+            image = URL(
+                "https://images.unsplash.com/photo-1766047125728-ebff5afcf314?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            ),
+            latitude = 0.0,
+            longitude = 0.0,
+            minimumMember = 20,
+            currentMember = 10,
+            basePrice = 50000,
+            discountRatio = 10.0,
+            discountedPrice = 45000,
+            deadLine = LocalDateTime.now(),
+            serviceTag = tags,
+        )
+
+        sleep(1000)
+        return Result.success(
+            SellerMainInfo(
+                companyName = "주식회사 싸비스",
+                ownerName = "홍길동",
+                phoneNumber = "010-1234-5678",
+                businessNumber = "123-45-67890",
+                description = "데모 판매자 정보입니다.",
+                services =
+                listOf(createService("서비스1"),
+                    createService("요가 클래스", listOf("힐링", "건강")))
+            )
+        )
+    }
+
+}
