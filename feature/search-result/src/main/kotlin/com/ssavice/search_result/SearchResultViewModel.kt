b99d4fa50c987c69542aff016347d95cf71ef64c
ksc1008 <ksc1008@naver.com>
2026-01-06 17:41:17 +0900

    [fix] 검색 결과 창에서 매 번 새로 검색하지 않게 수정

--- feature/search-result/src/main/kotlin/com/ssavice/search_result/SearchResultViewModel.kt
+++ feature/search-result/src/main/kotlin/com/ssavice/search_result/SearchResultViewModel.kt
@@ -18,58 +18,68 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchResultViewModel
-    @Inject
-    constructor(
-        private val savedStateHandle: SavedStateHandle,
-    ) : ViewModel() {
-        val queryState: StateFlow<SearchQuery> =
-            with(SearchResultRouteContract) {
-                combine(
-                    savedStateHandle.getStateFlow(SELECTED_CATEGORY, 0),
-                    savedStateHandle.getStateFlow(SEARCH_RANGE, 0),
-                    savedStateHandle.getStateFlow(START_PRICE, 0),
-                    savedStateHandle.getStateFlow(END_PRICE, 10_000_000),
-                    savedStateHandle.getStateFlow(SORT_BY, 0),
-                ) { category, searchRange, startPrice, endPrice, sortBy ->
-                    listOf(category, searchRange, startPrice, endPrice, sortBy)
-                }.combine(savedStateHandle.getStateFlow(QUERY, "")) { o1, query ->
-                    val q =
-                        SearchQuery(
-                            query = query,
-                            region1 = "region1",
-                            region2 = "region2",
-                            category = Category.entries.getOrElse(o1[0], { Category.entries[0] }),
-                            searchRange = o1[1],
-                            minPrice = o1[2],
-                            maxPrice = o1[3],
-                            sortBy = SortingOrder.entries.getOrElse(o1[4], { SortingOrder.POPULARITY }),
-                        )
-                    Log.d("query", q.toString())
-                    q
-                }.stateIn(
-                    scope = viewModelScope,
-                    started = SharingStarted.WhileSubscribed(5_000),
-                    initialValue =
-                        SearchQuery(
-                            query = "",
-                            region1 = "region1",
-                            region2 = "region2",
-                            category = Category.entries[0],
-                            searchRange = 0,
-                            minPrice = 0,
-                            maxPrice = 10_000_000,
-                            sortBy = SortingOrder.POPULARITY,
-                        ),
-                )
-            }
+@Inject
+constructor(
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    val queryState: StateFlow<SearchQuery> =
+        with(SearchResultRouteContract) {
+            combine(
+                savedStateHandle.getStateFlow(SELECTED_CATEGORY, 0),
+                savedStateHandle.getStateFlow(SEARCH_RANGE, 0),
+                savedStateHandle.getStateFlow(START_PRICE, 0),
+                savedStateHandle.getStateFlow(END_PRICE, 10_000_000),
+                savedStateHandle.getStateFlow(SORT_BY, 0),
+                savedStateHandle.getStateFlow(QUERY, "")
+            ) { params ->
+                val category = params[0] as Int
+                val searchRange = params[1] as Int
+                val startPrice = params[2] as Int
+                val endPrice = params[3] as Int
+                val sortBy = params[4] as Int
+                val query = params[5] as String
 
-        val uiState: StateFlow<SearchResultUiState> =
-            queryState
-                .transform {
-                    emit(SearchResultUiState(searchQuery = it))
-                }.stateIn(
-                    scope = viewModelScope,
-                    started = SharingStarted.WhileSubscribed(5_000),
-                    initialValue = SearchResultUiState(searchQuery = queryState.value),
-                )
+                val q =
+                    SearchQuery(
+                        query = query,
+                        region1 = "region1",
+                        region2 = "region2",
+                        category = Category.entries.getOrElse(category, { Category.entries[0] }),
+                        searchRange = searchRange,
+                        minPrice = startPrice,
+                        maxPrice = endPrice,
+                        sortBy = SortingOrder.entries.getOrElse(sortBy, { SortingOrder.POPULARITY }),
+                    )
+                Log.d("KSC", "query: $q")
+                q
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue =
+                    SearchQuery(
+                        query = "",
+                        region1 = "region1",
+                        region2 = "region2",
+                        category = Category.entries[0],
+                        searchRange = 0,
+                        minPrice = 0,
+                        maxPrice = 10_000_000,
+                        sortBy = SortingOrder.POPULARITY,
+                    ),
+            )
+        }
+
+    val uiState: StateFlow<SearchResultUiState> =
+        queryState
+            .transform {
+                emit(SearchResultUiState(searchQuery = it))
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = SearchResultUiState(searchQuery = queryState.value),
+            )
+
+    override fun onCleared() {
+        Log.d("KSC", "Search Result Cleared")
     }
+}
