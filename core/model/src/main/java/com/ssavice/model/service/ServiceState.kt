6157e152c5dfeb6a6c2c903a062157c63e866d3c
ksc1008 <ksc1008@naver.com>
2026-01-11 18:07:05 +0900

    [refactor] Enum 값 추가 밎 이동

---
+++ core/model/src/main/java/com/ssavice/model/service/ServiceState.kt
@@ -0,0 +1,9 @@
+package com.ssavice.model.service
+
+enum class ServiceState(val value: String) {
+    APPLYING("모집 중"),
+    MATCHED("모집 완료"),
+    FAILED("실패"),
+    CANCELED("취소됨"),
+    COMPLETED("완료됨")
+}
