c7b931ebf20cb71d7cbd768fc7e4d3346d63e232
권성찬 <54511614+ksc1008@users.noreply.github.com>
2026-01-15 12:51:43 +0900

    가입한 서비스 조회 API 연동 (#54)
    * [feat] 내 서비스 조회 기능 백엔드 API 연동
    
    * [chore] Convention format 수행

---
+++ core/network/src/main/java/com/ssavice/network/model/UserBookDTO.kt
@@ -0,0 +1,71 @@
+package com.ssavice.network.model
+
+import android.annotation.SuppressLint
+import com.ssavice.model.Date
+import com.ssavice.model.service.ServiceState
+import com.ssavice.model.user.UserServiceParticipation
+import com.ssavice.model.user.UserServiceParticipationItem
+import kotlinx.serialization.Serializable
+
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
+data class UserBookDTO(
+    val content: List<Content>,
+    val currentPage: Int,
+    val size: Int,
+    val totalElements: Long,
+    val totalPages: Int,
+) {
+    fun toModel(): UserServiceParticipation =
+        UserServiceParticipation(
+            items = content.map { it.toModel() },
+            currentPage = currentPage.toLong(),
+            searchCount = totalElements.toInt(),
+            hasNext = totalPages > currentPage,
+        )
+}
+
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
+data class Content(
+    val bookStatus: String,
+    val isReviewed: Boolean,
+    val serviceInfo: ServiceInfoDTO,
+) {
+    fun toModel(): UserServiceParticipationItem =
+        with(serviceInfo) {
+            UserServiceParticipationItem(
+                id = serviceId,
+                thumbnail = thumbnailUrl,
+                category = category,
+                price = discountedPrice.toInt(),
+                name = title,
+                sellerName = companyName,
+                startDate = Date.now(),
+                endDate = Date.now(),
+                state = ServiceState.valueOf(status),
+                isReviewed = isReviewed,
+            )
+        }
+}
+
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
+data class ServiceInfoDTO(
+    val basePrice: Long,
+    val category: String,
+    val companyId: Long,
+    val companyName: String,
+    val currentMember: Long,
+    val deadline: String,
+    val discountRate: Int,
+    val discountedPrice: Long,
+    val maximumMember: Long,
+    val minimumMember: Long,
+    val region: RegionDTO,
+    val serviceId: Long,
+    val status: String,
+    val tag: String,
+    val thumbnailUrl: String,
+    val title: String,
+)
