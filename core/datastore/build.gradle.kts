a312c5a6fe53fdabd6572b666a49d07c5033464a
ksc1008 <ksc1008@naver.com>
2026-01-18 14:04:04 +0900

    [chore] datastore 모듈 추가

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.ssavice.android.library)
+    alias(libs.plugins.ssavice.hilt)
+}
+android {
+    namespace = "com.ssavice.datastore"
+}
+
+dependencies {
+    api(projects.core.common)
+    implementation(projects.core.model)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore)
+    implementation(libs.tink)
+}
