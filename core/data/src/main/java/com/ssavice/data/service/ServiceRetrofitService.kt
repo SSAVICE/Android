835d47eb97301c8e957c89a68909ace60dc9cbdf
ksc1008 <ksc1008@naver.com>
2026-01-27 04:10:00 +0900

    [fix] 잘못된 DTO 타입 수정

--- core/data/src/main/java/com/ssavice/data/service/ServiceRetrofitService.kt
+++ core/data/src/main/java/com/ssavice/data/service/ServiceRetrofitService.kt
@@ -2,6 +2,7 @@ package com.ssavice.data.service
 
 import com.ssavice.network.model.AddServiceDTO
 import com.ssavice.network.model.AddServiceResponseDTO
+import com.ssavice.network.model.ApplyServiceResultDTO
 import com.ssavice.network.model.GetServiceDetailDTO
 import com.ssavice.network.model.ImageUploadDTO
 import com.ssavice.network.model.PresignedUrlResponseDTO
@@ -37,7 +38,7 @@ interface ServiceRetrofitService {
     @POST("/api/service/{id}/apply")
     suspend fun applyService(
         @Path(value = "id") id: Long,
-    ): Response<AddServiceResponseDTO>
+    ): Response<ApplyServiceResultDTO>
 
     @POST("/api/service/{id}/cancel")
     suspend fun cancelService(
