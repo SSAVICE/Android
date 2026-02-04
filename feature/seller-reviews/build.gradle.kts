c468fe8cb32e2cba638210750f5bec0e01a1cfc7
ksc1008 <ksc1008@naver.com>
2026-02-05 03:44:36 +0900

    [setting] 모듈 추가

---
+++ feature/seller-reviews/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.ssavice.android.library.compose)
+    alias(libs.plugins.ssavice.android.feature)
+}
+
+android {
+    namespace = "com.ssavice.feature.sellerreviews"
+}
+
+dependencies {
+    implementation(projects.core.data)
+}
