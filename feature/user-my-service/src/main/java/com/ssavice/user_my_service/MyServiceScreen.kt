f81065ca103f978332692a3c287d7910e74f8bc3
ksc1008 <ksc1008@naver.com>
2026-02-04 17:03:01 +0900

    [refactor] 내 서비스 목록에서 서비스 상태로 String 대신 ServiceState를 갖도록 수정

--- feature/user-my-service/src/main/java/com/ssavice/user_my_service/MyServiceScreen.kt
+++ feature/user-my-service/src/main/java/com/ssavice/user_my_service/MyServiceScreen.kt
@@ -15,7 +15,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
@@ -73,7 +77,13 @@ fun MyServiceScreen(
 ) {
     val isLoading =
         uiState.myServiceScreenStatus == MyServiceState.Loading ||
-            uiState.myServiceScreenStatus == MyServiceState.Initial
+                uiState.myServiceScreenStatus == MyServiceState.Initial
+    val serviceStates by remember {
+        derivedStateOf {
+            uiState.searchingState.map { it.value }
+        }
+    }
+
     InfiniteScrollContainer(
         modifier =
             modifier
@@ -86,7 +96,7 @@ fun MyServiceScreen(
             item {
                 ServiceStateFilter(
                     modifier = Modifier.fillMaxWidth(),
-                    searchRange = uiState.searchingState,
+                    searchRange = serviceStates,
                     selection = uiState.searchTypeSelection,
                     onSelectionChanged = onSearchingStateChanged,
                 )
