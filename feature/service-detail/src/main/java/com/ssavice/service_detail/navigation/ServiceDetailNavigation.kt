50d538d56a9e7f703c05d71b44841da965ec76a8
ksc1008 <ksc1008@naver.com>
2026-02-09 12:54:04 +0900

    [feat] navigation Route에 필드 추가

--- feature/service-detail/src/main/java/com/ssavice/service_detail/navigation/ServiceDetailNavigation.kt
+++ feature/service-detail/src/main/java/com/ssavice/service_detail/navigation/ServiceDetailNavigation.kt
@@ -21,15 +21,14 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ServiceDetailRoute(
     val serviceId: Long,
-    val sellerPage: Boolean = false,
+    val sellerPage: Boolean = false
 )
 
 fun NavController.navigateToServiceDetail(
     navOptions: NavOptionsBuilder.() -> Unit = {},
-    isSeller: Boolean = true,
     serviceId: Long,
 ) {
-    navigate(ServiceDetailRoute(serviceId, isSeller)) {
+    navigate(ServiceDetailRoute(serviceId)) {
         navOptions()
     }
 }
@@ -91,5 +90,5 @@ fun NavGraphBuilder.serviceDetailScreen(
 
 object ServiceDetailRouteContract {
     const val ID = "serviceId"
-    const val IS_SELLER = "sellerPage"
+    const val SELLER_PAGE = "sellerPage"
 }
