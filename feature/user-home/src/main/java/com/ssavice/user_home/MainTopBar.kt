be1b6c3f1502a2f6b4ea09e659714fc2e7a56f30
ksc1008 <ksc1008@naver.com>
2026-03-11 14:11:55 +0900

    [design] 메인 화면 주소 선택 버튼 UI 수정

--- feature/user-home/src/main/java/com/ssavice/user_home/MainTopBar.kt
+++ feature/user-home/src/main/java/com/ssavice/user_home/MainTopBar.kt
@@ -1,8 +1,12 @@
 package com.ssavice.user_home
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,8 +18,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -32,25 +38,38 @@ fun UserHomeTopBar(viewModel: UserHomeViewModel) {
                         contentColor = MaterialTheme.colorScheme.onSurface,
                     ),
             ) {
-                Text(
-                    text = (state.addressState as? RegionState.Showing)?.address ?: "",
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.SemiBold,
-                )
-                Icon(
-                    modifier = Modifier.size(32.dp),
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = "위치 선택",
-                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        modifier = Modifier.size(28.dp),
+                        imageVector = Icons.Default.LocationOn,
+                        contentDescription = "위치 아이콘"
+                    )
+                    Spacer(Modifier.width(5.dp))
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = (state.addressState as? RegionState.Showing)?.address ?: "",
+                        style = MaterialTheme.typography.titleLarge,
+                        maxLines = 1,
+                        fontWeight = FontWeight.SemiBold,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                    Spacer(Modifier.width(5.dp))
+                    Icon(
+                        modifier = Modifier.size(32.dp),
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = "",
+                    )
+                }
             }
         },
         actions = {
-            IconButton(onClick = viewModel::onNotificationButtonClick) {
-                Icon(
-                    Icons.Default.Notifications,
-                    contentDescription = "알림",
-                )
-            }
+            Spacer(Modifier.width(20.dp))
+            // IconButton(onClick = viewModel::onNotificationButtonClick) {
+            //     Icon(
+            //         Icons.Default.Notifications,
+            //         contentDescription = "알림",
+            //     )
+            // } TODO: 현재 비 활성화
         },
     )
 }
